790c89f86a162592fff8da8f80476d5c6dc1789c
Ed-CodePower <edwardcicioc@gmail.com>
2026-04-06 03:17:39 -0400

    update the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
+import Dashboard from "./pages/Dashboard"
+
 function App() {
   return (
     <div>
-      <h1>Task Manager</h1>
-      <p>React project started successfully.</p>
+      <Dashboard />
     </div>
   )
 }
