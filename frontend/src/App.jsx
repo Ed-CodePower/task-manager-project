33046571f344e3bbb795805251888909b5a545c0
Ed-CodePower <edwardcicioc@gmail.com>
2026-04-12 12:17:35 -0400

    Add Routes to each page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import Dashboard from "./pages/Dashboard"
+import { Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Dashboard />
+  return (
+    <Routes>
+      <Route path="/" elment={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
 }
 
 export default App
